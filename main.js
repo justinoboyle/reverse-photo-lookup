21aaedbc45c69f2c2a35bf8718d5a99fb97923d6
Justin <arraypro07@gmail.com>
2016-01-11 08:55:11 -0500

    Setut port properly

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + "/static/index.html");
 });
 app.use('/static', express.static('static'));
+
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Listening on port " + port);
+});
