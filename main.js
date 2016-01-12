eeb1eb63efbaa95a45c8589d3499e9970d7b7759
Justin <arraypro07@gmail.com>
2016-01-12 08:55:10 -0500

    Add basic proxy

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res) {
 });
 app.use('/static', express.static('static'));
 
+app.use('/proxy', function(req, res) {
+  var url = req.url.replace('/?url=','');
+  req.pipe(request(url)).pipe(res);
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
