c0ab77932ac944c4174b268080432f347ec6ebed
justinoboyle <arraypro07@gmail.com>
2016-01-18 15:09:59 -0500

    Basic functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var request = require('request');
 
 var app = express();
 
@@ -8,7 +9,19 @@ app.get('/', function(req, res) {
 app.use('/static', express.static('static'));
 
 app.use('/proxy', function(req, res) {
+  console.log(req.url);
   var url = req.url.replace('/?url=','');
+//  url = decodeURIComponent(url);
+  request.post(
+    url,
+    { },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+            return;
+        }
+    }
+);
   req.pipe(request(url)).pipe(res);
 });
 
